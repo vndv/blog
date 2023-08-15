9614705309a3a288659bc69073adf45743df2498
Ivan Matveev <imatveev@delimobil.ru>
2023-08-15 11:50:50 +0300

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   integrations: [tailwind()],
-  site: 'https://vndv.github.io'
+  site: 'https://vndv.github.io',
+  output: "server",
+  adapter: vercel()
 });
