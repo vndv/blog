36f76a5004958e5aae11b64c6f2d5d7e16854458
Ivan Matveev <imatveev@sberhealth.ru>
2025-02-19 14:51:43 +0300

    fix: update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vndv.vercel.app',
   integrations: [tailwind(), mdx()],
   content: {
     sources: ['content']
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,19 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function get(context) {
-  const blog = await getCollection('blog');
+export async function GET(context) {
+  const posts = await getCollection('blog');
+  
   return rss({
-    title: 'vndn',
-    description: 'Ivan Matveev personal blog',
+    title: 'Ivan Matveev Blog',
+    description: 'Блог о Data Engineering, больших данных и современных инструментах обработки данных',
     site: context.site,
-    items: blog.map((post) => ({
+    items: posts.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.pubDate,
+      pubDate: post.data.date,
       description: post.data.description,
-      customData: post.data.customData,
-      link: `/posts/${post.slug}/`,
+      link: `/blog/${post.slug}/`,
     })),
+    customData: `<language>ru-ru</language>`,
   });
 }
